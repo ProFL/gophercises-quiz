bc59a6e5be8880e9f4a678303f6a69931a5da0ca
Pedro F Linhares <pedro@profl.com.br>
2021-03-17 23:18:36 -0300

    :recycle: Improved error handling on read/parse

--- questions/parse_questions.go
+++ questions/parse_questions.go
@@ -11,10 +11,15 @@ import (
 
 func ReadAndParseQuestions(filePath string, shuffleQuestions bool) []QuestionAnswer {
 	file, err := os.Open(filePath)
-	defer file.Close()
 	if err != nil {
-		log.Panic("There was an error reading the questions file", err)
+		log.Panic("There was an error openning the questions file", err.Error())
 	}
+	defer func() {
+		close_err := file.Close()
+		if close_err != nil {
+			log.Println("Failed to close the questions file", close_err.Error())
+		}
+	}()
 	question_answers := parseQuestionsFromFile(file)
 	if shuffleQuestions {
 		rand.Seed(time.Now().UnixNano())
@@ -31,7 +36,7 @@ func parseQuestionsFromFile(file *os.File) []QuestionAnswer {
 	var question_answers []QuestionAnswer
 	for line != nil {
 		if err != nil {
-			log.Panic("Failed to parse questions file", err)
+			log.Panic("Failed to parse the questions file", err.Error())
 		}
 		question_answers = append(question_answers, QuestionAnswer{
 			question: line[0],
